422b430987a67b9b7c8eb59a206be1f8a0a48ebc
Milena Leme <milenaleme4@hotmail.com>
2020-09-22 11:02:57 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -10,10 +10,25 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 echo json_encode($usuarios);
 */
 
-$root = new Usuario();
+//carrega um usuario 
+//$root = new Usuario();
+//$root->loadById(5);
+//echo $root;
 
-$root->loadById(5);
+//carrega uma lista de usuario 
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
+//carrega uma lista de usuario buscando pelo login 
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//carrega um usuario usando o login e a senha 
+
+$usuario = new Usuario();
+$usuario->login("root", "!@#$");
+
+echo $usuario;
+
 
 ?>
